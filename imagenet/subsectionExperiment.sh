7ac59fff8e0d30d85d942e3dd08d3c61d3d7ba54
Pramit Mallick <pramitmallick@gmail.com>
2019-04-12 18:38:14 -0400

    proj

--- imagenet/subsectionExperiment.sh
+++ imagenet/subsectionExperiment.sh
@@ -13,8 +13,8 @@
 #SBATCH --cpus-per-task=28
 #SBATCH --exclusive
 #SBATCH --time=00:60:00
-#SBATCH --gres=gpu:k80:4
-##SBATCH --gres=gpu:p40:4
+##SBATCH --gres=gpu:k80:4
+#SBATCH --gres=gpu:p40:4
 #SBATCH --reservation=chung
 
 # module load pytorch/python2.7/0.3.0_4
@@ -24,6 +24,6 @@
 module load python3/intel/3.6.3 cuda/9.0.176 nccl/cuda9.0/2.4.2
 
 source ~/pytorch_env/py3.6.3/bin/activate
-echo "hw_16_0.001_alexnet_k80_loss_only"
-nvprof --timeout 1800 --output-profile alexnet_loss_only_16_0.001_small_k80_%p.nvvp --csv --log-file alexnet_loss_only_16_0.001_small_k80_%p.log --profile-from-start off --track-memory-allocations on --cpu-profiling on --print-summary python subsectionMain.py --arch alexnet -b 16 --epochs 1 --lr 0.001 /beegfs/work/courses/2019-CSCI-GA-3033-025/imagenet_pytorch_small
-# nvprof --output-profile alexnet_loss_only_16_0.001_small_k80_%p.nvvp --csv --log-file alexnet_loss_only_16_0.001_small_k80_%p.log --profile-from-start off --track-memory-allocations on --cpu-profiling on --print-summary python subsectionMain.py --arch alexnet -b 16 --epochs 1 --lr 0.001 /beegfs/work/courses/2019-CSCI-GA-3033-025/imagenet_pytorch_small
+echo "hw_16_0.001_alexnet_p40_loss_only"
+nvprof --timeout 1800 --output-profile alexnet_loss_only_16_0.001_small_p40_%p.nvvp --csv --log-file alexnet_loss_only_16_0.001_small_p40_%p.log --profile-from-start off --track-memory-allocations on --cpu-profiling on --print-summary python subsectionMain.py --arch alexnet -b 16 --epochs 1 --lr 0.001 /beegfs/work/courses/2019-CSCI-GA-3033-025/imagenet_pytorch_small
+# nvprof --output-profile alexnet_loss_only_16_0.001_small_p40_%p.nvvp --csv --log-file alexnet_loss_only_16_0.001_small_p40_%p.log --profile-from-start off --track-memory-allocations on --cpu-profiling on --print-summary python subsectionMain.py --arch alexnet -b 16 --epochs 1 --lr 0.001 /beegfs/work/courses/2019-CSCI-GA-3033-025/imagenet_pytorch_small
